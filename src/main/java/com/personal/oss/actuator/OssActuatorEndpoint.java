386da6d41b6154e30bd2744b07a9b9f8d929cb80
sunpeikai <spk123456>
2020-09-18 16:54:10 +0800

    oss actuator监控节点

--- src/main/java/com/personal/oss/actuator/OssActuatorEndpoint.java
+++ src/main/java/com/personal/oss/actuator/OssActuatorEndpoint.java
@@ -4,11 +4,10 @@
 package com.personal.oss.actuator;
 
 import com.aliyun.oss.OSSClient;
-import com.aliyun.oss.model.Bucket;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import com.personal.oss.utils.SpringUtils;
+import com.qcloud.cos.COSClient;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,29 +19,43 @@ import java.util.Map;
  * @version OssActuateEndpoint, v0.1 2020/9/18 09:47
  * @description
  */
-@Endpoint(id = "oss")
+@WebEndpoint(id = "oss")
 public class OssActuatorEndpoint {
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
     @ReadOperation
-    public Map<String, Object> invoke() {
-        System.out.println("run ... ");
+    public Map<String, Object> ossActuator() {
+        // 方法返回map
         Map<String, Object> result = new HashMap<>();
-        Map<String, OSSClient> ossClientMap = this.applicationContext.getBeansOfType(OSSClient.class);
-        int size = ossClientMap.size();
+        // oss节点数量
+        int size = 0;
+        // oss.info对象存储列表
         List<Object> ossClientList = new ArrayList<>();
-        ossClientMap.keySet().forEach((beanName) -> {
+
+        // 阿里云oss节点
+        Map<String, OSSClient> aliyunOSS = SpringUtils.getBeanOfType(OSSClient.class);
+        size += aliyunOSS.size();
+        aliyunOSS.keySet().forEach(beanName -> {
             Map<String, Object> ossProperties = new HashMap<>();
-            OSSClient client = (OSSClient)ossClientMap.get(beanName);
+            OSSClient client = aliyunOSS.get(beanName);
             ossProperties.put("beanName", beanName);
             ossProperties.put("endpoint", client.getEndpoint().toString());
             ossProperties.put("clientConfiguration", client.getClientConfiguration());
             ossProperties.put("credentials", client.getCredentialsProvider().getCredentials());
-            ossProperties.put("bucketList", client.listBuckets().stream().map(Bucket::getName).toArray());
+            ossProperties.put("bucketList", client.listBuckets().stream().map(com.aliyun.oss.model.Bucket::getName).toArray());
             ossClientList.add(ossProperties);
         });
+        // 腾讯云oss节点
+        Map<String, COSClient> tencentOSS = SpringUtils.getBeanOfType(COSClient.class);
+        size += tencentOSS.size();
+        tencentOSS.keySet().forEach(beanName ->{
+            Map<String, Object> ossProperties = new HashMap<>();
+            COSClient client = tencentOSS.get(beanName);
+            ossProperties.put("beanName", beanName);
+            ossProperties.put("clientConfig", client.getClientConfig().toString());
+            ossProperties.put("bucketList", client.listBuckets().stream().map(com.qcloud.cos.model.Bucket::getName).toArray());
+            ossClientList.add(ossProperties);
+        });
+
         result.put("size", size);
         result.put("info", ossClientList);
         return result;
